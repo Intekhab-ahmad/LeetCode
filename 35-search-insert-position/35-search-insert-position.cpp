348336efc8a46328b4ce3efe892723e5c75c398a
Intekhab_ahmad <intekhabahmad61@gmail.com>
2022-07-14 09:50:01 +0530

    Time: 4 ms (81.34%), Space: 9.7 MB (22.37%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -1,41 +1,29 @@
 class Solution {
-
 public:
-
-int searchInsert(vector<int> arr ,int k){
-
-
-if(arr[0]>k){
-        return 0;
+    int searchInsert(vector<int>& nums, int target) {
+        int start=0;
+        int end=nums.size()-1;
+      
+        int mid=(start+end)/2;
+        
+        while(start<end){
+            
+            if(nums[mid]==target)
+                return mid;
+            
+            else if(nums[mid]>target)
+                 end=mid-1;
+                
+            else
+               start=mid+1;
+                
+            mid=(start+end)/2;
+        }
+        if(nums[start]<target)
+            return {start+1};
+        else
+            return {start};
+        return end;
+        
     }
-int s=0;
-int e=arr.size();
-int mid=(s+e)/2;
-
-while(s<e){
-if(arr[mid]==k)
-
-  return mid;
-
-if(arr[mid]>k){
-
-// checking if the previous element is bigger 
-    
-  if(arr[mid-1]<k)
-    return mid;
-
-  // going left side
-  e=mid-1;
-}
-else{
-
-  //going to right
-
-  s=mid+1;
-}
- mid=(s+e)/2;
-}
-return e;
-}
-
 };
