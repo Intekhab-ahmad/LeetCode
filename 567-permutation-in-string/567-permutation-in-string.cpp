ea402d71f4cfc68979d1e7855a76cebb8c6b5881
Intekhab_ahmad <intekhabahmad61@gmail.com>
2022-09-08 20:06:15 +0530

    Time: 13 ms (60.25%), Space: 7.2 MB (74.48%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,36 +1,55 @@
 class Solution {
 public:
+    
+    bool checkEqual(int count1[26],int count2[26]){
+        for(int i=0;i<26;i++){
+            if(count1[i]!=count2[i])
+                return 0;
+            
+        }
+        return 1;
+    }
     bool checkInclusion(string s1, string s2) {
-                int n = s1.size(), m = s2.size(), j=0;
-        vector<int> a(26, 0), b(26, 0);
+        //character count array
+        int count1[26]={0};
         
-        if(n>m)
-            return false;
-        
-        else if(n<m){
-            for(auto x:s1)
-                a[x-'a']++;
-            for(int i=0; i<n; i++)
-                b[s2[i]-'a']++;
-            
-            if(a == b) return true;
-            
-            for(int i=n; i<m; i++){
-                b[s2[i]-'a']++;
-                b[s2[j]-'a']--;
-                if(a==b) return true;
-                j++;
-            }         
+        for(int i=0;i<s1.length();i++){
+            int index=s1[i]-'a';
+            count1[index]++;
         }
+        // traverse s2 string in window of size s1 length and compare
+        int i=0;
+        int windowsize=s1.length();
+        int count2[26]={0};
         
-        else{
-            for(auto x:s1)
-                a[x-'a']++;
-            for(auto x:s2)
-                b[x-'a']++;
-            if(a == b) return true;
+        //running for the first window
+        while(i<windowsize && i<s2.length()){
+            int index=s2[i]-'a';
+            count2[index]++;
+            i++;
         }
-        return false;
+        if(checkEqual(count1,count2))
+            return 1;
+        //aage ke window processing ke liy 
+        while(i<s2.length()){
+            
+            //naye character ko count2 array me update ...
+            char newchar=s2[i];
+            int index=s2[i]-'a';
+            count2[index]++;
+            
+            //puraaane character ka count hatana hai count2 array se..
+            
+            char oldchar=s2[i-windowsize];
+            index=oldchar-'a';
+            count2[index]--;
+            
+            i++;
+            
+            if(checkEqual(count1,count2))
+            return 1;
+        }
+        return 0;
         
     }
 };
