bc365c8343003e69408a6b007279c6c646ce8c18
Intekhab_ahmad <intekhabahmad61@gmail.com>
2023-01-10 22:48:04 +0530

    Time: 61 ms (53.13%), Space: 18.9 MB (95.87%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& nums) {
+        
+        vector<vector<int>> ans;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        
+        int start=nums[0][0];
+        int end=nums[0][1];
+        int i=0;
+        
+        for(i=1;i<n;i++){
+            if(nums[i][0]<=end){
+                end=max(end,nums[i][1]);
+                cout<<end<<endl;
+            }
+            else{
+                ans.push_back({start,end});
+                start=nums[i][0];
+                end=nums[i][1];
+            }
+            
+        }
+    cout<<start<<end<<i;
+        if(i==n){
+                ans.push_back({start,end});
+            }
+        return ans;
+    }
+};
