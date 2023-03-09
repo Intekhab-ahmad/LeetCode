76a438fbafa64a0a13e500ecd5bb33e9f50a66ca
Intekhab_ahmad <intekhabahmad61@gmail.com>
2023-03-09 15:36:11 +0530

    Time: 150 ms (66.05%), Space: 83.3 MB (83.74%) - LeetHub

--- 0775-global-and-local-inversions/0775-global-and-local-inversions.cpp
+++ 0775-global-and-local-inversions/0775-global-and-local-inversions.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
-    bool isIdealPermutation(vector<int>& A) {
-        int cmax = 0, n = A.size();
-        for (int i = 0; i < n - 2; ++i) {
-            cmax = max(cmax, A[i]);
-            if (cmax > A[i + 2]) return false;
+    bool isIdealPermutation(vector<int>& nums) {
+        
+        int n=nums.size();
+        int maxm=0;
+        
+        for(int i=0;i<n-2;i++){
+            
+            maxm=max(maxm,nums[i]);
+            if(maxm>nums[i+2])
+                return false;
+            
         }
         return true;
     }
-};
+};   
