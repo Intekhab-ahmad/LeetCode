bfcdb6d31ffb4681f176b5bb896bed7a2a13de70
Intekhab_ahmad <intekhabahmad61@gmail.com>
2023-03-13 23:27:32 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (22.93%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string interpret(string command) {
+        
+        string ans;
+        
+        for(int i=0; i<command.size(); i++){
+            if(command[i]=='G')
+                ans.push_back('G');
+            else if(command[i]=='(' && command[i+1]==')'){
+                ans.push_back('o');
+                i++;
+            }
+            else{
+                ans.push_back('a');
+                ans.push_back('l');
+                i=i+3;
+            }
+        }
+        return ans;
+        
+    }
+};
