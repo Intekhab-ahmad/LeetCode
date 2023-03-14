8aff4ae64633bfe97c450cd44e5a7425665bf2cf
Intekhab_ahmad <intekhabahmad61@gmail.com>
2023-03-15 01:05:17 +0530

    Time: 4 ms (91.58%), Space: 15.2 MB (51.48%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        
+        string str=s;
+        for(int i=0;i<indices.size();i++){
+            
+            str[indices[i]]=s[i];
+        }
+        cout<<str;
+        
+        return str;
+        
+        
+    }
+};
