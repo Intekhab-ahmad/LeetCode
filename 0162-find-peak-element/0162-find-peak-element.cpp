846718001422e68fd307c8194aed565d4632e4a8
Intekhab_ahmad <intekhabahmad61@gmail.com>
2023-03-25 12:24:00 +0530

    Time: 2 ms (87.71%), Space: 8.9 MB (32.00%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    int getpivot(vector<int>& nums){
+        
+        int s=0;
+        int e=nums.size()-1;
+        int mid=s+(e-s)/2;
+        
+        while(s<e){
+            
+            if(nums[mid]>nums[mid+1]){
+                e=mid;
+            }
+            else {
+                s=mid+1;
+            }
+            mid=s+(e-s)/2;
+            
+        }
+        return e;
+        
+        
+    }
+    int findPeakElement(vector<int>& nums) {
+        
+        
+        int a=getpivot(nums);
+        cout<<a;
+       
+        return getpivot(nums);
+        
+    }
+};
